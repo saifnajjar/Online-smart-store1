e4a44d8c5bbc1a3ad414c38d1d5efbe1cbc31b9d
saif najjar <saifnajar2000@gmail.com>
2023-07-11 04:44:15 -0700

    saifnajjar

--- white_lamp/settings.py
+++ white_lamp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
